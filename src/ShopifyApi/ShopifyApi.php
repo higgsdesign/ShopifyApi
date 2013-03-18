26da8eab67916624c5eb4e611d79117d952b44f4
Daniel Newns <dan@higgsdesign.com>
2013-03-18 09:46:12 +0000

    adding exceptions back
    Signed-off-by: Daniel Newns <dan@higgsdesign.com>

--- src/ShopifyApi/ShopifyApi.php
+++ src/ShopifyApi/ShopifyApi.php
@@ -123,3 +123,19 @@ class ShopifyApi {
     }
 
 }
+
+class CurlException extends \Exception { }
+
+class Exception extends \Exception
+{
+    protected $info;
+
+    function __construct($info)
+    {
+        $this->info = $info;
+        parent::__construct($info['response_headers']['http_status_message'], $info['response_headers']['http_status_code']);
+    }
+
+    function getInfo() { $this->info; }
+
+}
